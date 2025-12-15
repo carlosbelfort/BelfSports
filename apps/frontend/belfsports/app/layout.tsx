3e7d129758eb3a24d76c988d9ea9d9231d878499
carlosbelfort <carloslaurie1994@gmail.com>
2025-12-15 19:55:39 -0300

    Layout global
    Ajuste no layout global

--- apps/frontend/belfsports/app/layout.tsx
+++ apps/frontend/belfsports/app/layout.tsx
@@ -19,14 +19,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="pt-BR">
+      <body className="min-h-screen bg-black text-white">
         {children}
       </body>
     </html>
