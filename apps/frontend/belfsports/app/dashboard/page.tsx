44abe3d5510215d4bb81445a5d01f5e456154e0b
carlosbelfort <carloslaurie1994@gmail.com>
2025-12-15 19:44:30 -0300

    Inserção do logout
    Adição de um logout para remover informação de autenticação e retornar ao login

--- apps/frontend/belfsports/app/dashboard/page.tsx
+++ apps/frontend/belfsports/app/dashboard/page.tsx
@@ -1,23 +1,55 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 export default function DashboardPage() {
   const router = useRouter();
+  const [statusApi, setStatusApi] = useState('Verificando...');
 
   useEffect(() => {
     const auth = localStorage.getItem('auth');
 
     if (!auth) {
       router.push('/login');
+      return;
     }
+
+    async function checkApi() {
+      try {
+        const response = await axios.get('http://localhost:3333/health');
+        setStatusApi(response.data.message);
+      } catch {
+        setStatusApi('Erro ao conectar com a API');
+      }
+    }
+
+    checkApi();
   }, [router]);
+
+  function handleLogout() {
+    localStorage.removeItem('auth');
+    router.push('/login');
+  }  
+  
 
   return (
     <main className="p-6">
-      <h1 className="text-2xl font-bold">Dashboard</h1>
-      <p className="mt-2">Bem-vindo ao sistema.</p>
+        <div className="flex itens-center justify-between">
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+
+             <button 
+                onClick={handleLogout}
+                className="rounded bg-red-600 px-4 py-2 text-white hover:bg-red-700">
+                Logout
+             </button>
+
+        </div>
+     
+      <p className="mt-4">
+        <strong>Status da API:</strong> {statusApi}
+      </p>
     </main>
   );
 }
